b7e86783bc8fb4348d8e08eed6a41a699ef7cf71
Your Name <you@example.com>
2023-09-09 11:35:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: All time 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') (putchar(b));
+				
+	putchar('\n');
+	return (0);
+}
