8fcf079d4e96bcf5622ae090cde87dedce553736
Your Name <you@example.com>
2023-09-10 13:31:10 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,24 +3,20 @@
  * main - prints all possible different combinations of two digits
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
+	int a;
 
-	for (tens = '0'; tens <= '9'; tens++)
-
-		for (ones = '0'; tens <= '9'; ones++)
-
-		       while (!((ones == tens) || (tens > ones)))
-
-				putchar(tens);
-				putchar(ones);
-			        while (!(ones == '9' && tens == '8'))
-
-					putchar(',');
-					putchar(' ');
-
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
