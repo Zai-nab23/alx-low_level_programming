069085d81d7808851fb683e1d11d4427ea518444
Your Name <you@example.com>
2023-09-13 13:23:01 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer -  prints every minute of the day of Jack Bauer
- * Return: void
+ * Return: ...
  */
 void jack_bauer(void)
 {
@@ -10,7 +10,7 @@ void jack_bauer(void)
 
 	for (a = 0; a < 24; a++)
 	{
-		for (b = 60; b < 60; b++)
+		for (b = 0; b < 60; b++)
 		{
 			_putchar((a / 10) + '0');
 			_putchar((a % 10) + '0');
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * time_table - Entry point for 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (r
+
