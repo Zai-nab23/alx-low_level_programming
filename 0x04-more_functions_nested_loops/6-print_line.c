cd06bab47ec01085cd2f8820a522d0b16328f5b5
Your Name <you@example.com>
2023-09-15 12:53:55 -0700

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: input integer
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+}
