1ecb4ff5c425065f38688e3c4849236c324d5ebe
Your Name <you@example.com>
2023-09-28 07:37:02 -0700

    6- is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function that returns 1
+ * if the input integer is a prime number
+ * @n: input integer
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	return (0);
+	else
+	return (1);
+}
