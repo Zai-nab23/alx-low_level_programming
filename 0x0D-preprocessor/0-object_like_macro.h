eaacc8433ceecf30fd82f733c45bec4839ebf8b0
Your Name <you@example.com>
2023-10-10 12:30:07 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,10 +2,10 @@
 #define MACRO_H
 
 /**
- * file: Create a header file that defines a macro name
- *  0-object_like_macro.h
- *  @size: 1024
+ * file: 0-object_like_macro.h
+ * Create a header file that defines a macro name
  *  describtion: header files
  */
 #define SIZE 1024
+
 #endif
