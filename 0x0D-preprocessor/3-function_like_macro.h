e71f7dc08a8a51417e65b514b9b6381b0d1d5769
Your Name <you@example.com>
2023-10-10 11:55:16 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,7 +5,7 @@
  * file: 3-function_like_macro.h
  * function-like macro ABS(x) that computes
  * the absolute value of a number x
- * @x: value 
+ * @x: value
  * Describtion: header file
  */
 #define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
