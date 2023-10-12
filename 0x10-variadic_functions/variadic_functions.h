2f1e8485f7463318e1190069d9d6c23e1c780ced
Your Name <you@example.com>
2023-10-12 10:38:38 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,10 @@
-#pragma once
+#ifndef _VARIADIC_FUNCTION_
+#define _VARIADIC_FUNCTION_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
 
 int sum_them_all(const unsigned int n, ...);
 
+#endif
