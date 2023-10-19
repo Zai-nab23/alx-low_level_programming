e2da82374166b1dbf66870aed7f11aa3e7f84ba7
Your Name <you@example.com>
2023-10-19 10:24:48 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * list_len - function that returns the number of
+ * elements in a linked list_t list
+ * @h: pointer to the first node
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	int a;
+
+	if (h == NULL)
+		return (0);
+
+	for (a = 1; h->next != NULL; a++)
+		h = h->next;
+	return (a);
+}
