661daeea161d52d46fabf588c1b919abbecc9313
Your Name <you@example.com>
2023-10-20 04:25:49 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void print_first(void)__attribute__((constructor));
+/**
+ * print_first - function that print first before main is executed
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
