b615c0f9c1dc64c0ddc37345fe759c133c3623ba
Your Name <you@example.com>
2023-10-24 15:35:42 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	for (a = 0; a < index -1 && initial != NULL && index != 0; a++)
+	for (a = 0; a < index - 1 && initial != NULL && index != 0; a++)
 		initial = head->next;
 	if (initial == NULL)
 		return (-1);
