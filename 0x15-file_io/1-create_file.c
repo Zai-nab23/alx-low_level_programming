eb7d4467143407551e1a1bfb6ae521f370ab6eb0
Your Name <you@example.com>
2023-10-30 11:00:53 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	filediscribtor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	filediscribtor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (filediscribtor == -1)
 		return (-1);
