842e2318e70c7b49d8c00efad8095e4eb7960d8b
Your Name <you@example.com>
2023-10-30 12:33:25 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_file - function that appends text at the end of a file
+ * @filename: name of file content
+ * @text_content: content of appended file
+ * Return: Return 1 if the file exists and -1 if the file does not exist
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int filediscribtor;
+	int readwritten;
+	int newletters;
+
+	if (!filename)
+		return (-1);
+	filediscribtor = open(filename, O_WRONLY | O_APPEND);
+		if (filediscribtor == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (newletters = 0; text_content[newletters]; newletters++)
+			;
+		readwritten = write(filediscribtor, text_content, newletters);
+
+		if (readwritten == -1)
+			return (-1);
+	}
+	close(filediscribtor);
+		return (1);
+}
