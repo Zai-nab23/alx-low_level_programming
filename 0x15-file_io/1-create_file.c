89c0e45f57852c4cdb86a455874cf6799a7d53ae
Your Name <you@example.com>
2023-10-30 10:45:59 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (newletters = 0; text_content[newletters]; newletters++)
+		;
 
 	readwritten = write(filediscribtor, text_content, newletters);
 		if (readwritten == -1)
